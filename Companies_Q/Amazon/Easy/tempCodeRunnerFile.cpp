486e40f93cabd7c395210f595f1595e3133bca70
BHANUPRATAP SINGH <bhanurjb21@gmail.com.com>
2025-09-23 01:32:19 +0530

    commit

---
+++ Companies_Q/Amazon/Easy/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+void removeduplicate(vector<int> &arr){
+//     for(int i=0; i< arr.size(); i++){
+//         for(int j=i+1; j<arr.size(); j++){
+//             if(arr[i]==arr[j]){
+//                 arr.erase(arr.begin() + j);
+//                 j--;
+//             }
+//         }
+//     }
+// }
