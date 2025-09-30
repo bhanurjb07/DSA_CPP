4b1acf1ef0cfa4a3f383d03a4862a6208270489a
BHANUPRATAP SINGH <bhanurjb21@gmail.com>
2025-10-01 04:28:06 +0530

    commit

--- Pointers/pointers.cpp
+++ Pointers/pointers.cpp
@@ -1,5 +1,13 @@
 #include<iostream>
 using namespace std;
-int main(){
+int main(){ 
+    int a =10;
+    int *ptr = &a;
+    int **pptr = &ptr;
+    cout<<&a <<" = "<< ptr << endl;
+    cout<< pptr <<" = "<< &ptr << endl;
+    //Dereference
+    cout<< *(&a)<<endl;
     return 0;
 }
+//Special operator that stores address of the other variables...
