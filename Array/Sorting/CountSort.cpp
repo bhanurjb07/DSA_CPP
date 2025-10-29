4635737df2fcb965f67a950019605825f220f681
BHANUPRATAP SINGH <bhanurjb21@gmail.com>
2025-10-29 11:20:42 +0530

    basic

---
+++ Array/Sorting/CountSort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+void CountSort(int arr[], int n){
+    int k = INT_MIN;
+    for(int i=0; int<n; i++){
+        k = max(k,arr[i]);
+    }
+    for(int i=0; i<n; i++){
+        ount[];
+        ++Count[arr[i]];
+    }
+}
+void printarray(int arr[], int n){
+    for(int i =0; i<n; i++){
+        cout<<arr[i];
+    }
+}
+int main(){
+    int arr[]={1,2,5,6,3,4,6,5,4,3};
+    int n = sizeof(arr)/sizeof(int);
+    return 0;
+}
