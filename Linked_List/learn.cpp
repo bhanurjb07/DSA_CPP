153916c66c37107c208bcff4e87b3c2bd72fa90e
BHANUPRATAP SINGH <bhanurjb21@gmail.com>
2025-10-29 12:29:50 +0530

    insertion

--- Linked_List/learn.cpp
+++ Linked_List/learn.cpp
@@ -18,6 +18,38 @@ void InsertAtHead(Node* &head, int d){
     temp-> next = head;
     head = temp;
 }
+
+void InsertAtTail(Node* &tail, int d){
+    Node* temp = new Node(d);
+    tail->next =temp;
+    tail = temp;
+}
+
+void InsertAtPosition(Node* &tail, Node* &head, int pos, int d){
+    //insert at start
+    if(pos == 1){
+        InsertAtHead(head, d);
+        return;
+    }
+    Node* temp = head;
+    int cnt = 1;
+    while(cnt < pos -1){
+        temp = temp -> next;
+        cnt++;
+    }
+
+    //inserint at last position
+    if(temp-> next == NULL){
+        InsertAtTail(tail, d);
+        return;
+    }
+
+    //cranyeating a node
+    Node* nodetoinsert = new Node(d);
+    nodetoinsert -> next = temp->next;
+    temp->next = nodetoinsert;
+
+}
 void print(Node* &head){
     Node* temp = head;
 
@@ -32,12 +64,20 @@ int main(){
     Node* node1 = new Node(10);
     cout << node1 -> data <<endl;
     cout<<node1 -> next<<endl;
+
     //head pointed to node
+    Node* tail = node1;
     Node* head = node1;
     print(head);
     
     InsertAtHead(head,12);
     print(head);
+    InsertAtHead(head,15);
+    print(head);
+    InsertAtTail(tail,20);
+    print(head);
 
+    InsertAtPosition(tail,head, 1, 22);
+    print(head);
     return 0;
 }
